1f81a2dc24ae30c4dfd69ae3612dbc087e4722c4
Vladislav-Ciuperca <ciuperca37@gmail.com>
2024-05-06 19:24:15 +0200

    creo object in js

---
+++ myscript/script.js
@@ -0,0 +1,12 @@
+const team = {
+    "wayne barnet": {
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        immagine: "wayne-barnett-founder-ceo.jpg",
+    },
+    "Angela Caroll": {
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor",
+        immagine: "angela-caroll-chief-editor.jpg",
+    },
+}
