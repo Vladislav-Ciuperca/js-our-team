0084fa84f89a6dd4f1971cbdb410151668ee6176
Vladislav-Ciuperca <ciuperca37@gmail.com>
2024-05-06 19:54:50 +0200

    creo un ciclo che legge i avri contenuti

--- myscript/script.js
+++ myscript/script.js
@@ -1,12 +1,42 @@
-const team = {
-    "wayne barnet": {
+
+
+const team = [
+    {
         nome: "Wayne Barnett",
         ruolo: "Founder & CEO",
-        immagine: "wayne-barnett-founder-ceo.jpg",
+        immagine: "img/wayne-barnett-founder-ceo.jpg",
     },
-    "Angela Caroll": {
+    {
         nome: "Angela Caroll",
         ruolo: "Chief Editor",
-        immagine: "angela-caroll-chief-editor.jpg",
+        immagine: "img/angela-caroll-chief-editor.jpg",
     },
+    {
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
+        immagine: "img/walter-gordon-office-manager.jpg",
+    },
+    {
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        immagine: "img/angela-lopez-social-media-manager.jpg",
+    },
+    {
+        nome: "Developer",
+        ruolo: "Chief Editor",
+        immagine: "img/scott-estrada-developer.jpg",
+    },
+    {
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        immagine: "img/barbara-ramos-graphic-designer.jpg",
+    },
+]
+
+
+for (let i = 0; i < team.length; i++) {
+    const membro = team[i];
+    console.log(membro.nome)
+    console.log(membro.cognome)
+    console.log(membro.immagine)
 }
